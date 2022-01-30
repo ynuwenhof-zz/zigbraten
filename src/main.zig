043644bbc03e05cdde32d5c32a1719dd816a62a1
Yannic Nuwenhof <ynuwenhof@pm.me>
2022-01-30 15:04:11 +0100

    Add thread entry point skeleton

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,33 @@
 const std = @import("std");
 const windows = std.os.windows;
+const kernel32 = windows.kernel32;
 
 const BOOL = windows.BOOL;
 const DWORD = windows.DWORD;
 const LPVOID = windows.LPVOID;
+const HMODULE = windows.HMODULE;
 const HINSTANCE = windows.HINSTANCE;
 
 const dll_process_attach = 1;
 
-pub export fn DllMain(_: HINSTANCE, fdw_reason: DWORD, _: LPVOID) BOOL {
+extern "kernel32" fn DisableThreadLibraryCalls(hLibModule: HMODULE) callconv(WINAPI) BOOL;
+
+pub export fn DllMain(hinst_dll: HINSTANCE, fdw_reason: DWORD, _: LPVOID) BOOL {
     switch (fdw_reason) {
-        dll_process_attach => {},
+        dll_process_attach => {
+            _ = DisableThreadLibraryCalls(@ptrCast(HMODULE, hinst_dll));
+
+            const optional_handle = kernel32.CreateThread(null, 0, entry, hinst_dll, 0, null);
+            if (optional_handle) |handle| {
+                _ = kernel32.CloseHandle(handle);
+            }
+        },
         else => {},
     }
 
     return 1;
+}
+
+export fn entry(_: *anyopaque) DWORD {
+    return 0;
 }
